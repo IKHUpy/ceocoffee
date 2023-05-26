7c800859064e219163af7ac4914e9920ff7a20fd
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2023-05-26 23:42:47 +0800

    still unfinised

--- GROUP PT/login.js
+++ GROUP PT/login.js
@@ -1,13 +1,20 @@
+let userss = JSON.parse(localStorage.getItem('userss')) || [];
+
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-  
-    var username = document.getElementById('username').value;
+
+    var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-  
-    if (username == 'admin' && password == 'admin') {
-      alert('Logged in successfully');
+
+    // Find the user with the matching email and password
+    var user = userss.find(function(user) {
+        return user.email === email && user.password === password;
+    });
+
+    // If a matching user is found or the inputs are 'admin', allow login
+    if (user || (email === 'admin' && password === 'admin')) {
+        alert('Logged in successfully');
     } else {
-      alert('Invalid credentials');
+        alert('Invalid credentials');
     }
-  });
-  
+});
--- GROUP PT/register.js
+++ GROUP PT/register.js
@@ -1,19 +1,52 @@
-let user = JSON.parse(localStorage.getItem('user')) || [];
+let userss = JSON.parse(localStorage.getItem('userss')) || [];
+
   document.getElementById('registeruser').addEventListener('click', function (event){
-    let fullname = document.getElementById('fullname').value;
-    let password = document.getElementById('password').value;
-    let address = document.getElementById('address').value;
-    let city = document.getElementById('city').value;
-    let phone = document.getElementById('phone').value;
-    let email = document.getElementById('email').value;
-    let zip = document.getElementById('zip').value;
-    let cardnumber = document.getElementById('cardnumber').value;
-    let valid = document.getElementById('valid').value;
-    let cvv = document.getElementById('cvv').value;
-    if (fullname && password && address && city && phone && email && zip && cardnumber && valid && cvv){
+    event.preventDefault();
+    if (document.getElementById('fullname').value && document.getElementById('password').value &&
+    document.getElementById('address').value && document.getElementById('city').value && 
+    document.getElementById('phone').value && document.getElementById('email').value&&
+    document.getElementById('zip').value && document.getElementById('cardnumber').value &&
+    document.getElementById('valid').value &&  document.getElementById('cvv').value
+    ){
+      fetch('users.json')
+      .then(response => response.json())
+      .then(data=>{
+        
+        let i = 0;
+        let idFound = false;
+        console.log(data);
+        while (!idFound) {
+            idFound = true;
+            data.users.forEach(user1 => {
+                if (user1.id === i) {
+                    idFound = false;
+                }
+            });
+            if (!idFound) {
+                i++;
+            }
+        }
+        let newdata = {
+          id: i,
+          password: document.getElementById('password').value,
+          email: document.getElementById('email').value,
+          fullname: document.getElementById('fullname').value,
+          address: document.getElementById('address').value,
+          city: document.getElementById('city').value,
+          phone: document.getElementById('phone').value,
+          zipcode: document.getElementById('zip').value,
+          cardnumber: document.getElementById('cardnumber').value,
+          validthru: document.getElementById('valid').value,
+          ccv: document.getElementById('cvv').value
+        };
+        
+        userss.push(newdata);
+        localStorage.setItem('userss', JSON.stringify('userss'));
+        console.log(userss);
+        })
       
     } else {
-      alert('Please fill up the empty spaces, they are all REQUIRED');
+      alert('Please fill up the empty spaces, they are all REQUIRED')
     }
 
   });

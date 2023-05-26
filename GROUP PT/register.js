a0b8beb8b9b9e3da50c84c66d7dc2501938c3f5b
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2023-05-26 21:25:37 +0800

    not finished

--- GROUP PT/register.js
+++ GROUP PT/register.js
@@ -1,14 +1,19 @@
-document.getElementById('newUser').addEventListener('click', function(event) {
-    event.preventDefault();
-  
-    // Here you might redirect the user to a sign up page
-    alert('New User functionality is not implemented in this example');
+let user = JSON.parse(localStorage.getItem('user')) || [];
+  document.getElementById('registeruser').addEventListener('click', function (event){
+    let fullname = document.getElementById('fullname').value;
+    let password = document.getElementById('password').value;
+    let address = document.getElementById('address').value;
+    let city = document.getElementById('city').value;
+    let phone = document.getElementById('phone').value;
+    let email = document.getElementById('email').value;
+    let zip = document.getElementById('zip').value;
+    let cardnumber = document.getElementById('cardnumber').value;
+    let valid = document.getElementById('valid').value;
+    let cvv = document.getElementById('cvv').value;
+    if (fullname && password && address && city && phone && email && zip && cardnumber && valid && cvv){
+      
+    } else {
+      alert('Please fill up the empty spaces, they are all REQUIRED');
+    }
+
   });
-  
-  document.getElementById('forgotPassword').addEventListener('click', function(event) {
-    event.preventDefault();
-  
-    // Here you might redirect the user to a password recovery page
-    alert('Forgot Password functionality is not implemented in this example');
-  });
-  

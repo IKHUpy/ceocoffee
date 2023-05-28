36e0f0dbd2afff2e3f132332dcc5396b5c3fc9ac
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2023-05-28 20:01:47 +0800

    fixed that stupid tray page

--- GROUP PT/checkoutscript.js
+++ GROUP PT/checkoutscript.js
@@ -1,43 +1,46 @@
 let cart = JSON.parse(localStorage.getItem('cart'));
+console.log(cart);
 window.onload = function() {
     let itemhtml = '';
     let val = 0;
     let vat = 0;
     let pay = 0;
     cart.forEach(item => {
-        itemhtml += `
-        <div class="linear-box">
-            <div>
-            <img src="${item.img}">
-            </div> 
-            <div class="box-element" style='padding-top: '>
-                <div>Item name:
-                    <h4> ${item.name}<h4>
+        if (item){
+            itemhtml += `
+            <div class="linear-box">
+                <div>
+                <img src="${item.img}">
+                </div> 
+                <div class="box-element" style='padding-top: '>
+                    <div>Item name:
+                        <h4> ${item.name}<h4>
+                    </div>
+                    <div>Ice level: 
+                        <p>${item.iceLevel}</p> 
+                    </div>
+                    <div>Brew: 
+                        <p>${item.temperature}</p> 
+                    </div>
+                    <div>Sugar level: 
+                        <p>${item.sugarLevel}</p> 
+                    </div>
+                    <div>Cup size: 
+                        <p>${item.cupSize}</p> 
+                    </div>
+                    <div>Price: 
+                        <p> <sup>&#x20B1;</sup>${item.price}</p> 
+                    </div>
+                    <div>Quantity: 
+                        <p>${item.quantity}</p> 
+                    </div>
+                    
                 </div>
-                <div>Ice level: 
-                    <p>${item.iceLevel}</p> 
-                </div>
-                <div>Brew: 
-                    <p>${item.temperature}</p> 
-                </div>
-                <div>Sugar level: 
-                    <p>${item.sugarLevel}</p> 
-                </div>
-                <div>Cup size: 
-                    <p>${item.cupSize}</p> 
-                </div>
-                <div>Price: 
-                    <p> <sup>&#x20B1;</sup>${item.price}</p> 
-                </div>
-                <div>Quantity: 
-                    <p>${item.quantity}</p> 
-                </div>
-                
             </div>
-        </div>
-        `;
+            `;
 
-        pay += parseFloat(item.price) * parseFloat(item.quantity);
+            pay += parseFloat(item.price) * parseFloat(item.quantity);
+        }   
     });
     val = pay * 0.88;
     vat = pay * 0.12;
